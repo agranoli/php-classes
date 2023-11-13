38bc632048528fa90d304f515cdcbf452d8f9506
Reinis Artimovics <124902374+agranoli@users.noreply.github.com>
2023-11-13 13:42:36 +0200

    sdf

--- main.cpp
+++ main.cpp
@@ -1,26 +1,51 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-class Car{
+class Vehicle{
     public:
         string brand;
-        Car(string brand){
+        Vehicle(string brand) {
             this->brand = brand;
         }
-        Car (string brand, int mileage){
+        Vehicle(int mleage){
+            this->mileage = mileage;
+        }
+        Vehicle(string brand, int mileage) {
             this->brand = brand;
             this->mileage = mileage;
         }
-        ~Car(){
-            cout << this->brand << "NIGGABALZZZZZZZZZZZ" <<endl;
+        ~Vehicle() {
+            cout << this->brand << " is dead" <<endl;
         }
+        virtual void makeNoise() = 0;
+
     private:
         int mileage = 0;
 };
 
+
+class Car : public Vehicle {
+public:
+    Car(string brand) : Vehicle(brand) {
+    };
+    Car(int mileage) : Vehicle(mileage){
+
+    };
+    Car(string brand, int mileage) : Vehicle (brand, mileage){
+    };
+    void makeNoise(){
+        cout << "Beep beep, Imzi \n";
+    }
+};
+
 int main() {
-    Car* myCar = new Car("Dacia", 19000);
-    cout << myCar->brand << endl;
+    Car myCar("Dacia", 19200);
+    Car* myCar2 = new Car (19200);
+    delete myCar2;
+    cout << "Hello world! \n";
+
+    return 0;
 }
